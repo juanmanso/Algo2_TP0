f944fbd7d40140a9f76175562033efaceb67717f
Ricardo Andreasen <ricardoandreasen95@gmail.com>
2016-09-23 18:54:55 -0300

    Empezo a funcar ameo

--- Programa/test/am_proc.cc
+++ Programa/test/am_proc.cc
@@ -16,29 +16,36 @@ void am_proc(istream *is, ostream *os, const size_t& n_decimator){
 
 
 	// Si entra un archivo vacio (primero lee EOF), corta el for y luego el while, devolviendo un vacio
-	
+
+	cout<<"HOLA"<<endl;
+
 	while(!eof_flag){
 		
 		//  Se suman los primeros 'n_decimator' números hasta que corte
-		for(i=1; i<=n_decimator && !(*is>>aux); i++)
+		for(i=1; i<=n_decimator && ((*is)>>aux); i++){
 			c += aux;
+			*os<<"c: "<<c<<endl;
+			*os<<aux<<endl;
+		}
 	
 		// Compruebo si se llegó a EOF
-		if(is->eof())
-			eof_flag=true;
-
-		else{ 
-		// El for terminó por no puder guardar el caracter en aux_x
-			cerr	<< "Error: Cannot find EOF on input stream"
-				<< endl;
-			exit(1);
-		}		
+//		if(i == n_decimator){
+			if(is->eof())
+				eof_flag=true;
+	
+			if(!(is->good())){ 
+			// El for terminó por no puder guardar el caracter en aux_x
+				cerr	<< "Error: Cannot find EOF on input stream"
+					<< endl;
+				exit(1);
+			}		
+//		}
 
 		// Realizo el promedio móvil
 		c=c / n_decimator;
 			
 		// Imprimo el valor absoluto
-		*os << c.abs();
+		*os << c.abs()<<endl;
 	}
 	
 	if(os->bad()){
